841b7156611abe27e802ffb4e50f3eae1551d2f2
SaiSakthi18 <136805612+SaiSakthi18@users.noreply.github.com>
2023-10-14 23:01:33 +0530

    Update DC.sh

--- DC.sh
+++ DC.sh
@@ -19,13 +19,13 @@ else
 fi
 
 # Output CSV file in the output directory
-output_csv="output/output.csv"
+output_csv="output/DiscardedCases.csv"
 
 # Output Excel file in the output directory
-output_excel="output/output.xlsx"
+output_excel="output/DiscardedCases.xlsx"
 
-# Log file in the output directory
-log_file="output/script_log.txt"
+# Log file in the logs directory
+log_file="logs/DiscardedCases.txt"
 
 # Function to log errors and exit with an error code
 log_error_and_exit() {
@@ -34,14 +34,47 @@ log_error_and_exit() {
   exit 1
 }
 
+# Function to log query issues and errors and exit with an error code
+log_query_error_and_exit() {
+  local error_message="$1"
+  echo "SQL Query Error: $error_message" >> "$log_file"
+  exit 2
+}
+
+# Log entry: Starting SQL query execution
+echo "Starting SQL query execution..." >> "$log_file"
+
 # Execute SQL query and save result as CSV
-sqlcmd -S "$server,$port" -d "$database" -U "$username" -P "$password" -Q "$query" -o "$output_csv" -s "," -W -w 700 2>> "$log_file" || log_error_and_exit "SQL query execution failed."
+sqlcmd -S "$server,$port" -d "$database" -U "$username" -P "$password" -Q "$query" -o "$output_csv" -s "," -W -w 700 2>> "$log_file" || log_query_error_and_exit "SQL query execution failed."
+
+# Log entry: SQL query executed successfully
+echo "SQL query executed successfully." >> "$log_file"
+
+# Check if the output file is empty (query returned no results)
+if [ ! -s "$output_csv" ]; then
+  log_query_error_and_exit "SQL query returned no results."
+fi
+
+# Log entry: SQL query returned results
+echo "SQL query returned results." >> "$log_file"
+
+# Log entry: Removing hyphens from the CSV file
+echo "Removing hyphens from the CSV file..." >> "$log_file"
 
 # Remove hyphens after the header using sed
-sed -i '/^--/d' "$output_csv" || log_error_and_exit "Hyphen removal failed."
+sed -i '/^--/d' "$output_csv" || log_query_error_and_exit "Hyphen removal failed."
+
+# Log entry: Hyphens removed from the CSV file
+echo "Hyphens removed from the CSV file." >> "$log_file"
+
+# Log entry: Converting CSV to Excel
+echo "Converting CSV to Excel..." >> "$log_file"
 
 # Convert CSV to Excel using LibreOffice
-libreoffice --convert-to xlsx "$output_csv" --outdir "output" 2>> "$log_file" || log_error_and_exit "CSV to Excel conversion failed."
+libreoffice --convert-to xlsx "$output_csv" --outdir "output" 2>> "$log_file" || log_query_error_and_exit "CSV to Excel conversion failed."
+
+# Log entry: CSV converted to Excel successfully
+echo "CSV converted to Excel successfully." >> "$log_file"
 
 # Clean up the CSV file (optional)
 rm "$output_csv"
