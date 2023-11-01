a291a5b4f7333f0af601558ee5478e35efea64aa
SaiSakthi18 <136805612+SaiSakthi18@users.noreply.github.com>
2023-11-01 12:45:53 +0530

    Update Apc.sh

--- Apc.sh
+++ Apc.sh
@@ -7,6 +7,7 @@ current_date=$(date +'%Y%m%d')
 output_dir="output"
 log_file="logs/autopc_${current_date}.log"
 output_file="${output_dir}/AutoPC_${current_date}.xlsx"
+csv_file="${output_dir}/AutoPC_${current_date}.csv"
 
 # Database connection details (including the port)
 db_server="your_db_server"
@@ -25,30 +26,29 @@ remove_old_files() {
 # Execute the stored procedure and save the result as CSV
 execute_stored_procedure() {
     local procedure_name="your_stored_procedure_name"
-    local csv_file="${output_dir}/AutoPC_${current_date}.csv"
 
-    # Execute the stored procedure using sqlcmd
+    # Get column names from the stored procedure result
+    column_names=$(sqlcmd -S "${db_server},${db_port}" -d "${db_name}" -U "${db_user}" -P "${db_password}" -Q "SET NOCOUNT ON; EXEC $procedure_name" -W -h-1 -t " " | head -n 1)
+
+    # Execute the stored procedure and append the result to the CSV file
     echo "Executing stored procedure..."
-    sqlcmd -S "${db_server},${db_port}" -d "${db_name}" -U "${db_user}" -P "${db_password}" -Q "EXEC $procedure_name" -o "$csv_file" -h -1 -s ","
+    sqlcmd -S "${db_server},${db_port}" -d "${db_name}" -U "${db_user}" -P "${db_password}" -Q "SET NOCOUNT ON; EXEC $procedure_name" -s "," -h -1 >> "$csv_file"
 
     # Check for sqlcmd errors
     if [ $? -ne 0 ]; then
         echo "Error: Failed to execute the stored procedure."
         exit 1
     fi
+
+    # Add the column names as the header to the CSV file
+    sed -i "1s/^/${column_names}\n/" "$csv_file"
 }
 
 # Convert CSV to XLSX using LibreOffice
 convert_csv_to_xlsx() {
-    # Check if LibreOffice is installed
-    if ! command -v libreoffice &> /dev/null; then
-        echo "Error: LibreOffice is not installed."
-        exit 1
-    fi
-
     # Convert CSV to XLSX
     echo "Converting CSV to XLSX..."
-    libreoffice --headless --convert-to xlsx --outdir "$output_dir" "${output_dir}/AutoPC_${current_date}.csv" 2>> "$log_file"
+    libreoffice --headless --convert-to xlsx --outdir "$output_dir" "$csv_file" 2>> "$log_file"
 }
 
 # Main script
@@ -62,7 +62,7 @@ main() {
     # Remove old files
     remove_old_files
 
-    # Execute the stored procedure
+    # Execute the stored procedure and retrieve column names
     execute_stored_procedure
 
     # Convert CSV to XLSX
